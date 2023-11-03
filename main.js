be87ba67a918f98d826101dbc5a8f921950416dc
Kiameg <kiax1262@stud.kea.dk>
2023-11-03 14:33:51 +0100

    nye

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+import { gsap } from "gsap";
+import Letterize from "letterizejs";
+
+const text = new Letterize({ targets: ".animation div", wrapper: "div" });
+
+let tl = gsap.timeline();
+tl.from(text.listAll, { x: -4000, duration: 1.5, stagger: 0.1 });
+tl.to(".animation", { opacity: 0, duration: 1, onComplete: () => document.querySelector(".animation").remove() }, "+=0.5");
+
+gsap.from(".vs_icons", { x: -4000, duration: 1 });
