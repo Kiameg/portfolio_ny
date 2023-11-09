55aceef231d94e4ffe38a5b601a80b3cff1af376
Kiameg <kiax1262@stud.kea.dk>
2023-11-09 23:43:58 +0100

    Fix src

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ document.querySelector(".aboutme_btn").addEventListener("click", aboutMe);
 document.querySelector(".back_btn").addEventListener("click", showPage);
 
 function showPage() {
-  console.log("showPage");
   document.querySelector(".about_me").classList.add("hide");
   document.querySelector(".back_btn").classList.add("hide");
   document.querySelector(".aboutme_btn").classList.remove("hide");
@@ -18,7 +17,6 @@ function showPage() {
 }
 
 function aboutMe() {
-  console.log("aboutMe");
   document.querySelector(".column4_grid").classList.add("hide");
   document.querySelector(".aboutme_btn").classList.add("hide");
   document.querySelector(".about_me").classList.remove("hide");
